bd279a9dbe31ea771ee5e7ff469df3acf98fa2dc
caijiazhu <caijiazhu126@126.com>
2016-02-15 09:30:38 +0800

    [caijiazhu] add some files
    Signed-off-by: caijiazhu <caijiazhu126@126.com>

---
+++ gl2_basic/jni/gl2_basic.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#include <EGL/egl.h>
+#include <EGLUtils.h>
+
+using namespace android;
+
+static void checkEglError(const char* op, EGLBoolean returnVal = EGL_TRUE)
+{
+    if (returnVal != EGL_TRUE)
+    {
+        fprintf(stderr, "%s() returned %d\n", op, returnVal);
+    }
+    
+    for (EGLint error = eglGetError(); error != EGL_SUCCESS; error = eglGetError())
+    {
+        fprintf(stderr, "after %s() eglError %s (0x%x)\n", op, EGLUtils::strerror(error), error);
+    }
+}
+
+int printEGLConfigurations(EGLDisplay dpy)
+{
+    EGLint numConfig = 0;
+    EGLint returnVal = eglGetConfigs(dpy, NULL, 0, &numConfig);
+    if (EGL_FALSE == returnVal)
+    {
+        
+        return false;
+    }
+}
+
+int main()
+{
+    EGLDisplay dpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (EGL_NO_DISPLAY == dpy)
+    {
+        printf("eglGetDisplay returned EGL_NO_DISPLAY.\n");
+        return 0;
+    }
+    
+    EGLint majorVersion, minorVersion;
+    EGLBoolean returnValue = eglInitialize(dpy, &majorVersion, &minorVersion);
+    if (EGL_FALSE == returnValue)
+    {
+        printf("eglInitialize failed.\n");
+        return 0;    
+    }
+    
+    printEGLConfigurations(dpy);
+    
+    return 0;
+}

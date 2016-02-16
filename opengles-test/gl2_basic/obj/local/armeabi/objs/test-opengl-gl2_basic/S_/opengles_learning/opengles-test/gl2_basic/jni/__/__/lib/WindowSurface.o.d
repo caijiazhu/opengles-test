dc9149fe898f5e3c4ff26c116173d12459148e5b
caijiazhu <caijiazhu126@126.com>
2016-02-16 14:56:25 +0800

    [caijiazhu] remove file WindowSurface.h
    Signed-off-by: caijiazhu <caijiazhu126@126.com>

---
+++ opengles-test/gl2_basic/obj/local/armeabi/objs/test-opengl-gl2_basic/S_/opengles_learning/opengles-test/gl2_basic/jni/__/__/lib/WindowSurface.o.d
@@ -0,0 +1,35 @@
+S:/opengles_learning/opengles-test/gl2_basic/obj/local/armeabi/objs/test-opengl-gl2_basic/S_/opengles_learning/opengles-test/gl2_basic/jni/__/__/lib/WindowSurface.o: \
+ S:/opengles_learning/opengles-test/gl2_basic/jni/../../lib/WindowSurface.cpp \
+ S:/opengles_learning/opengles-test/gl2_basic/jni/../../include/WindowSurface.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/EGL/egl.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/EGL/eglplatform.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/KHR/khrplatform.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/api-level.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/stdint.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/machine/wchar_limits.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/native_window.h \
+ E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/rect.h
+
+S:/opengles_learning/opengles-test/gl2_basic/jni/../../include/WindowSurface.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/EGL/egl.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/EGL/eglplatform.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/KHR/khrplatform.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/api-level.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/stdint.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/machine/wchar_limits.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/native_window.h:
+
+E:/learning/Android/android-studio/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/rect.h:
